1908ea104791bda5abdd100f6ee7cc8585829b31
Tom Wright <tom@inflatablecookie.com>
2018-04-19 22:47:42 +0100

    Refactored Error factory into instanciatable class
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/lang/error/Factory.php
+++ libraries/lang/error/Factory.php
@@ -10,7 +10,116 @@ use df\core;
 
 class Factory
 {
+    const STANDARD = [
+        'ELogic' => [
+            'type' => 'LogicException'
+        ],
+            'EBadFunctionCall' => [
+                'extend' => 'ELogic',
+                'type' => 'BadFunctionCallException'
+            ],
+                'EBadMethodCall' => [
+                    'extend' => 'EBadFunctionCall',
+                    'type' => 'BadMethodCallException'
+                ],
+
+            'EDomain' => [
+                'extend' => 'ELogic',
+                'type' => 'DomainException'
+            ],
+            'EInvalidArgument' => [
+                'extend' => 'ELogic',
+                'type' => 'InvalidArgumentException'
+            ],
+            'ELength' => [
+                'extend' => 'ELogic',
+                'type' => 'LengthException'
+            ],
+            'EOutOfRange' => [
+                'extend' => 'ELogic',
+                'type' => 'OutOfRangeException'
+            ],
+
+            'EDefinition' => [
+                'extend' => 'ELogic'
+            ],
+            'EImplementation' => [
+                'extend' => 'ELogic'
+            ],
+                'ENotImplemented' => [
+                    'extend' => 'EImplementation',
+                    'http' => 501
+                ],
+
+            'EUnsupported' => [
+                'extend' => 'ELogic'
+            ],
+
+
+        'ERuntime' => [
+            'type' => 'RuntimeException'
+        ],
+            'EOutOfBounds' => [
+                'extend' => 'ERuntime',
+                'type' => 'OutOfBoundsException'
+            ],
+            'EOverflow' => [
+                'extend' => 'ERuntime',
+                'type' => 'OverflowException'
+            ],
+            'ERange' => [
+                'extend' => 'ERuntime',
+                'type' => 'RangeException'
+            ],
+            'EUnderflow' => [
+                'extend' => 'ERuntime',
+                'type' => 'UnderflowException'
+            ],
+            'EUnexpectedValue' => [
+                'extend' => 'ERuntime',
+                'type' => 'UnexpectedValueException'
+            ],
+
+            'EBadRequest' => [
+                'extend' => 'ERuntime',
+                'http' => 400
+            ],
+            'EUnauthorized' => [
+                'extend' => 'ERuntime',
+                'http' => 401
+            ],
+            'EForbidden' => [
+                'extend' => 'EUnauthorized',
+                'http' => 403
+            ],
+            'ENotFound' => [
+                'extend' => 'ERuntime',
+                'http' => 404
+            ],
+            'EComponentUnavailable' => [
+                'extend' => 'ERuntime'
+            ],
+            'EServiceUnavailable' => [
+                'extend' => 'ERuntime',
+                'http' => 503
+            ]
+    ];
+
+    const REWIND = 4;
+
     private static $instances = [];
+
+    protected $type;
+    protected $params = [];
+
+    protected $namespace;
+
+    protected $interfaces = [];
+    protected $traits = [];
+
+    protected $exceptionDef;
+    protected $interfaceDefs = [];
+
 
     /**
      * Generate a context specific, message oriented throwable error
@@ -22,193 +131,258 @@ class Factory
             $message = $message['message'] ?? 'Undefined error';
         }
 
-        $params['rewind'] = $rewind = max((int)($params['rewind'] ?? 0), 0);
-        $activeRewind = $rewind + 3;
-        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $activeRewind);
-        $namespace = null;
-        $activeRewind--;
+        return (new self($type, $params))
+            ->build($message, $interfaces);
+    }
 
-        if(isset($trace[$activeRewind])) {
-            $namespace = $trace[$activeRewind]['class'] ?? null;
-        }
 
-        $activeRewind--;
+    protected function __construct(?string $type, array $params=[])
+    {
+        $this->type = $type;
+        $this->params = $params;
+    }
 
-        if (isset($params['namespace'])) {
-            $namespace = $params['namespace'];
-            unset($params['namespace']);
-        } else {
-            if (!empty($namespace)) {
-                if (false !== strpos($namespace, 'class@anon')) {
-                    $namespace =  '\\df';
+
+
+    /**
+     * Build exception object
+     */
+    protected function build(string $message, array $interfaces): IError
+    {
+        $this->params['rewind'] = $rewind = max((int)($this->params['rewind'] ?? 0), 0);
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $rewind + static::REWIND);
+        $key = $rewind + static::REWIND - 1;
+
+        if (isset($this->params['namespace'])) {
+            $this->namespace = $this->params['namespace'];
+            unset($this->params['namespace']);
+        } elseif (isset($trace[$key])) {
+            $this->namespace = $trace[$key]['class'] ?? null;
+
+            if (!empty($this->namespace)) {
+                if (false !== strpos($this->namespace, 'class@anon')) {
+                    $this->namespace = '\\df';
                 } else {
-                    $namespace = explode('\\', $namespace);
-                    $className = array_pop($namespace);
-                    $namespace = implode('\\', $namespace);
+                    $parts = explode('\\', $this->namespace);
+                    $className = array_pop($parts);
+                    $this->namespace = implode('\\', $parts);
                 }
-            } else {
-                $namespace = '\\df';
             }
         }
 
-        $defs = static::buildDefinitions($type, $interfaces, $namespace);
-        $exceptionDef = $defs['@exception'];
-        unset($defs['@exception']);
+        if (empty($this->namespace)) {
+            $this->namespace = '\\df';
+        }
 
-        foreach ($defs as $def) {
+        $this->buildDefinitions($interfaces);
+
+        foreach ($this->interfaceDefs as $def) {
             eval($def);
         }
 
-        $hash = md5($exceptionDef);
+        $hash = md5($this->exceptionDef);
 
         if (!isset(self::$instances[$hash])) {
-            self::$instances[$hash] = eval($exceptionDef);
+            self::$instances[$hash] = eval($this->exceptionDef);
         }
 
         $trace = array_pop($trace);
 
-        if (!isset($params['file'])) {
-            $params['file'] = $trace['file'];
+        if (!isset($this->params['file'])) {
+            $this->params['file'] = $trace['file'];
         }
 
-        if (!isset($params['line'])) {
-            $params['line'] = $trace['line'];
+        if (!isset($this->params['line'])) {
+            $this->params['line'] = $trace['line'];
         }
 
-        return new self::$instances[$hash]($message, $params);
+        return new self::$instances[$hash]($message, $this->params);
+    }
+
+
+    protected function buildDefinitions(array $interfaces): void
+    {
+        $namespace = ltrim($this->namespace, '\\');
+        $namespaces = [$namespace];
+        $directType = null;
+        $this->traits[] = 'df\\lang\\error\\TError';
+
+        // Ensure root IError exists
+        if (!interface_exists('\\df\\IError')) {
+            $this->interfaceDefs['df\\IError'] = 'namespace df;interface IError extends \\df\\lang\\error\\IError {};';
+        }
+
+        // Create initial interface list
+        foreach ($interfaces as $i => $interface) {
+            if (false !== strpos($interface, '/')) {
+                $interface = 'df\\'.str_replace('/', '\\', ltrim($interface, '/'));
+            }
+
+            $interface = ltrim($interface, '\\');
+
+            if (false === strpos($interface, '\\')) {
+                $interface = $namespace.'\\'.$interface;
+            }
+
+            if (null !== ($ns = $this->listInterface($interface))) {
+                $namespaces[] = $ns;
+            }
+        }
+
+
+        // Create inheritance trees
+        foreach (array_unique($namespaces) as $namespace) {
+            $this->extractNamespaceInterfaces($namespace);
+        }
+
+
+        // Sort inheritance list
+        foreach ($this->interfaces as $interface => $info) {
+            if ($info !== null) {
+                $this->defineInterface($interface, $info);
+            }
+        }
+
+        // Ensure defaults
+        if ($this->type === null) {
+            $this->type = \Exception::class;
+        }
+
+        if (empty($this->interfaces)) {
+            $this->interfaces['df\\IError'] = [];
+        }
+
+
+        // Build class def
+        $this->exceptionDef = 'return new class(\'\') extends '.$this->type;
+
+        if (!empty($this->interfaces)) {
+            $this->exceptionDef .= ' implements '.implode(',', array_keys($this->interfaces));
+        }
+
+        $this->exceptionDef .= ' {';
+
+        foreach ($this->traits as $trait) {
+            $this->exceptionDef .= 'use '.$trait.';';
+        }
+
+        $this->exceptionDef .= '};';
+    }
+
+
+
+    /**
+     * Add interface info to class extend list
+     */
+    protected function listInterface(string $interface): ?string
+    {
+        $parts = explode('\\', $interface);
+        $name = array_pop($parts);
+        $output = null;
+
+        if ($name !== 'IError' && !preg_match('/^(E)[A-Z][a-zA-Z0-9_]+$/', $name)) {
+            return null;
+        }
+
+        if (!interface_exists($interface, true)) {
+            $output = implode('\\', $parts);
+
+            if (isset(static::STANDARD[$name])) {
+                $standard = static::STANDARD[$name];
+
+                if (isset($standard['extend'])) {
+                    $standard['extend'] = ['df\\'.$standard['extend']];
+                }
+
+                $this->interfaces[$interface] = $standard;
+
+                if ($this->type === null && isset($standard['type'])) {
+                    $this->type = $standard['type'];
+                }
+
+                if (!isset($this->params['http']) && isset($standard['http'])) {
+                    $this->params['http'] = $standard['http'];
+                }
+            } elseif (!isset($this->interfaces[$interface])) {
+                $this->interfaces[$interface] = [];
+            }
+
+            if ($name === 'IError') {
+                array_pop($parts);
+            }
+
+            $extend = implode('\\', $parts).'\\IError';
+
+            if (count($parts) > 1 && $extend !== 'df\\lang\\error\\IError') {
+                $this->interfaces[$interface]['extend'][] = $extend;
+            }
+        } else {
+            $this->interfaces[$interface] = null;
+        }
+
+        return $output;
     }
 
 
     /**
-     * Create eval definitions for the class and interfaces
+     * Create an interface tree back down to df ns root
      */
-    public static function buildDefinitions(?string $type, array $interfaces, string $namespace): array
+    protected function extractNamespaceInterfaces(string $namespace): void
     {
-        $traits = $defs = [];
-        $namespace = ltrim($namespace, '\\');
-        $namespaces = [$namespace];
+        $parts = explode('\\', $namespace);
+        $parts = array_slice($parts, 1, 3);
+        $parent = 'df';
 
-        if(!interface_exists('\\df\\IError')) {
-            $defs['df\\IError'] = 'namespace df;interface IError extends \\df\\lang\\error\\IError {};';
+        foreach ($parts as $part) {
+            $first = $parent == 'df';
+            $ins = $parent.'\\'.$part;
+            $interface = $ins.'\\IError';
+
+            if (interface_exists($interface) || isset($this->interfaces[$interface])) {
+                return;
+            }
+
+            $this->listInterface($interface);
+            $parent = $ins;
         }
+    }
 
-        $traits[] = 'df\\lang\\error\\TError';
 
-        if (!empty($interfaces)) {
-            foreach ($interfaces as $i => $interface) {
-                if (false !== strpos($interface, '/')) {
-                    $interface = 'df\\'.str_replace('/', '\\', ltrim($interface, '/'));
-                }
+    /**
+     * Recursively define interfaces, adding in inherited parents
+     */
+    protected function defineInterface(string $interface, array $info): void
+    {
+        $parent = '\\df\\IError';
 
-                $interface = ltrim($interface, '\\');
+        if (isset($info['extend'])) {
+            $parent = [];
 
-                if (false === strpos($interface, '\\')) {
-                    $interface = $namespace.'\\'.$interface;
-                } else {
-                    $parts = explode('\\', $interface);
-                    $test = '\\df\\lang\\'.array_pop($parts);
-                }
-
-                if (!interface_exists($interface, true)) {
-                    $parts = explode('\\', $interface);
-                    $name = array_pop($parts);
-
-                    if (($parts[1] ?? 'lang') !== 'lang') {
-                        $namespaces[] = implode('\\', $parts);
-                    }
-
-                    $defs[$interface] = 'namespace '.implode($parts, '\\').';interface '.$name.' extends \\df\\IError {}';
-                }
-
-                if ($type === null) {
-                    $baseName = '\\'.substr($name, 1).'Exception';
-
-                    if (class_exists($baseName)) {
-                        $type = $baseName;
-                    }
-                }
-
-                $interfaces[$i] = $interface;
-                $parts = explode('\\', $interface);
+            foreach ($info['extend'] as $extend) {
+                $parent[] = '\\'.$extend;
+                $parts = explode('\\', $extend);
                 $name = array_pop($parts);
 
-                if (!preg_match('/^E[A-Z][a-zA-Z0-9_]+$/', $name)) {
-                    unset($interfaces[$i]);
-                    continue;
-                }
-
-                $traitName = implode('\\', $parts).'\\T'.substr($name, 1);
-
-                if (trait_exists($traitName, true)) {
-                    $traits[] = $traitName;
+                if (isset($this->interfaces[$extend])) {
+                    $inner = $this->interfaces[$extend];
+                    unset($this->interfaces[$extend]);
+                    $this->defineInterface($extend, $inner);
+                } elseif (isset(static::STANDARD[$name])) {
+                    $this->defineInterface($extend, static::STANDARD[$name]);
                 }
             }
+
+            $parent = implode(',', $parent);
         }
 
-        foreach (static::extractNamespaceInterfaces(...array_unique($namespaces)) as $interface => $def) {
-            $interfaces[] = $interface;
+        $parts = explode('\\', $interface);
+        $name = array_pop($parts);
+        $traitName = implode('\\', $parts).'\\T'.substr($name, 1);
 
-            if ($def !== null) {
-                $defs[$interface] = $def;
-            }
+        if (trait_exists($traitName, true)) {
+            $this->traits[] = $traitName;
         }
 
-        if(empty($interfaces)) {
-            $interfaces[] = IError::class;
-        }
-
-        if ($type === null) {
-            $type = \Exception::class;
-        }
-
-        $definition = 'return new class(\'\') extends '.$type;
-
-        if (!empty($interfaces)) {
-            $definition .= ' implements '.implode(',', array_unique($interfaces));
-        }
-
-        $definition .= ' {';
-
-        foreach ($traits as $trait) {
-            $definition .= 'use '.$trait.';';
-        }
-
-        $definition .= '};';
-        $defs['@exception'] = $definition;
-
-        return $defs;
-    }
-
-
-
-    /**
-     * Create an interface tree for the current namespace
-     */
-    private static function extractNamespaceInterfaces(string ...$namespaces): array
-    {
-        $extra = [];
-
-        foreach ($namespaces as $namespace) {
-            $parts = explode('\\', $namespace);
-            $parts = array_slice($parts, 1, 3);
-            $parent = 'df';
-
-            foreach ($parts as $part) {
-                $first = $parent == 'df';
-                $ins = $parent.'\\'.$part;
-                $interface = $ins.'\\IError';
-                $interfaceDef = null;
-
-                if (!interface_exists($interface, true)) {
-                    $base = $first ? '\\df\\IError' : '\\'.$parent.'\\IError';
-                    $interfaceDef = 'namespace '.$ins.';interface IError extends '.$base.' {}';
-                }
-
-                $parent = $ins;
-                $extra[$interface] = $interfaceDef;
-            }
-        }
-
-        return $extra;
+        $this->interfaceDefs[$interface] = 'namespace '.implode($parts, '\\').';interface '.$name.' extends '.$parent.' {}';
     }
 }
