a895f2a0d305f5e02d1c70c5b3a50fc901ed725b
Tom Wright <tom@inflatablecookie.com>
2018-04-20 13:41:25 +0100

    Updated dumper colours
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/lang/debug/dumper/HtmlDumper.php
+++ libraries/lang/debug/dumper/HtmlDumper.php
@@ -13,7 +13,7 @@ use Symfony\Component\VarDumper\Dumper\HtmlDumper as SymfonyHtmlDumper;
 class HtmlDumper extends SymfonyHtmlDumper
 {
     protected $styles = [
-        'default' => 'background-color:#fff; color:#888; line-height:1.2; font-weight:normal; font:12px Monaco, Consolas, monospace; word-wrap: break-word; white-space: pre-wrap; position:relative; z-index:100000',
+        'default' => 'background-color:#fff; color:#bbb; line-height:1.2; font-weight:normal; font:12px Monaco, Consolas, monospace; word-wrap: break-word; white-space: pre-wrap; position:relative; z-index:100000',
         'num' => 'color:#a814e3',
         'const' => 'color:#b50acc',
         'str' => 'color:#cc2123',
