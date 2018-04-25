7fa3ef4e3ad50a713f09043dbf75acb954f4f7b4
Tom Wright <tom@inflatablecookie.com>
2018-04-25 13:44:24 +0100

    Added extra helpers to loader
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/core/ILoader.php
+++ libraries/core/ILoader.php
@@ -3,13 +3,17 @@
  * This file is part of the Decode Framework
  * @license http://opensource.org/licenses/MIT
  */
-namespace df\core\loader;
+namespace df\core;
 
 use df;
 
 interface ILoader
 {
     public function loadPackages(array $packages): void;
+
+    public function getBasePath(): string;
+    public function getVendorPath(): string;
+
     public function getApexPaths(): array;
     public function getLibraryPaths(): array;
 }
--- libraries/core/loader/Composer.php
+++ libraries/core/loader/Composer.php
@@ -11,29 +11,37 @@ use df\core;
 
 use Composer\Autoload\ClassLoader;
 
-class Composer implements ILoader
+class Composer implements core\ILoader
 {
     const APEX = [
         'http', 'themes'
     ];
 
-    public $autoload;
-
-    public $vendorPath;
-    public $basePath;
+    protected $vendorPath;
+    protected $basePath;
 
     protected $apexPaths = [];
     protected $libraryPaths = [];
 
+    protected $autoload;
+
+
+    /**
+     * Construct with default Composer ClassLoader
+     */
     public function __construct(ClassLoader $autoload)
     {
-        $this->autoload = $autoload;
-
         $reflection = new \ReflectionClass(ClassLoader::class);
         $this->vendorPath = dirname(dirname($reflection->getFileName()));
         $this->basePath = dirname($this->vendorPath);
+
+        $this->autoload = $autoload;
     }
 
+
+    /**
+     * Take a list of package names and install them into the loader
+     */
     public function loadPackages(array $packages): void
     {
         $this->apexPaths = [$this->basePath];
@@ -53,11 +61,36 @@ class Composer implements ILoader
         $this->autoload->setPsr4('df\\', $this->libraryPaths);
     }
 
+
+    /**
+     * Path to app/
+     */
+    public function getBasePath(): string
+    {
+        return $this->basePath;
+    }
+
+    /**
+     * Path to app/vendor
+     */
+    public function getVendorPath(): string
+    {
+        return $this->vendorPath;
+    }
+
+
+
+    /**
+     * List of package paths for app specific files
+     */
     public function getApexPaths(): array
     {
         return $this->apexPaths;
     }
 
+    /**
+     * List of package paths for library class files
+     */
     public function getLibraryPaths(): array
     {
         return $this->libraryPaths;
