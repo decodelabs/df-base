1a36e8fbae46f4c9b3863639d47be6dcb255d0f6
Tom Wright <tom@inflatablecookie.com>
2018-05-19 15:19:34 +0100

    Added normalizer to time interval
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Time/Interval.php
+++ src/Time/Interval.php
@@ -13,6 +13,35 @@ use Carbon\CarbonInterval;
 class Interval extends CarbonInterval
 {
     /**
+     * Ensure input is either instance of Interval or null
+     */
+    public static function normalizeInstance($duration): ?Interval
+    {
+        if ($duration === null) {
+        } elseif (is_int($duration)) {
+            return static::seconds($duration);
+        } elseif (is_string($duration)) {
+            return static::fromString($duration);
+        } elseif ($duration instanceof \DateInterval) {
+            return static::instance($duration);
+        } elseif (is_array($duration)) {
+            return static::create(...$duration);
+        } else {
+            throw Df\Error::EInvalidArgument('Invalid duration format', null, $duration);
+        }
+    }
+
+    /**
+     * Normalize carry over points
+     */
+    public function normalize(): Interval
+    {
+        $date = new \DateTime();
+        $date->add($this);
+        return static::instance($date->diff(new \DateTime()));
+    }
+
+    /**
      * Dump info
      */
     public function __debugInfo(): array
