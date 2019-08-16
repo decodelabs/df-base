3284c56710424c4a7f68c29797d5c1e6707a5b21
Tom Wright <tom@inflatablecookie.com>
2019-08-16 11:08:07 +0100

    Fixed Symphony dumper
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Lang/Dumper/Symfony.php
+++ src/Lang/Dumper/Symfony.php
@@ -34,29 +34,26 @@ class Symfony implements IDumper
         if ('cli' === PHP_SAPI) {
             $dumper = new CliDumper();
         } else {
-            $dumper = new class() extends HtmlDumper {
-                protected $styles = [
-                    'default' => 'background-color:#fff; color:#bbb; line-height:1.2; font-weight:normal; font:12px Monaco, Consolas, monospace; word-wrap: break-word; white-space: pre-wrap; position:relative; z-index:100000',
-                    'num' => 'color:#a814e3',
-                    'const' => 'color:#b50acc',
-                    'str' => 'color:#cc2123',
-                    'cchr' => 'color:#222',
-                    'note' => 'color:#0cb300',
-                    'ref' => 'color:#a0a0a0',
-                    'public' => 'color:#795da3',
-                    'protected' => 'color:#795da3',
-                    'private' => 'color:#795da3',
-                    'meta' => 'color:#0cb300',
-                    'key' => 'color:#df5000',
-                    'index' => 'color:#a71d5d',
-                ];
-            };
-
-            /*
-            $dumper->setDisplayOptions([
-                'maxDepth' => 10
+            $dumper = new HtmlDumper();
+            $dumper->setStyles([
+                'default' => 'background-color:#fff; color:#bbb; line-height:1.2; font-weight:normal; font:12px Monaco, Consolas, monospace; word-wrap: break-word; white-space: pre-wrap; position:relative; z-index:100000',
+                'num' => 'color:#a814e3',
+                'const' => 'color:#b50acc',
+                'str' => 'color:#cc2123',
+                'cchr' => 'color:#222',
+                'note' => 'color:#0cb300',
+                'ref' => 'color:#a0a0a0',
+                'public' => 'color:#795da3',
+                'protected' => 'color:#795da3',
+                'private' => 'color:#795da3',
+                'meta' => 'color:#0cb300',
+                'key' => 'color:#df5000',
+                'index' => 'color:#a71d5d',
             ]);
-            */
+
+            $dumper->setDisplayOptions([
+                'maxDepth' => 3
+            ]);
         }
 
 
